154882714e24d8a59d782c8b46bc9429c678a894
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-01-15 18:00:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 2432;
+        int bonus = 20;
+
+        int miles = priceTicket / bonus;
+
+        System.out.println("Начислено: " + miles + " миль(я)");
+    }
+}
